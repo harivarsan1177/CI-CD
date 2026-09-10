d490fa28bdc7cf7752eb751dd46f989209078cc8
harivarsan1177 <harivarsan1177@gmail.com>
2026-09-10 22:14:42 +0530

    Question 2

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-def add(a, b): 
-    return a - b
-def subtract(a, b):
-    return a - b
+def find_max(numbers):
+    return max(numbers)
+
+def count_evens(numbers):
+    return len([n for n in numbers if n % 2 == 0])
